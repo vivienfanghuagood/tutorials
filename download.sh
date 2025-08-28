ef5e321849ea2545374956f5b89a09014704c40d
vivienfanghua <vivienfanghua@163.com>
2025-08-28 09:00:04 +0000

    add

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 rm -rf *.ipynb
 wget https://huggingface.co/Qwen/Qwen3-Embedding-8B.ipynb
 wget https://huggingface.co/JungZoona/T3Q-qwen2.5-14b-v1.0-e3.ipynb
-wget https://huggingface.co/openai/whisper-large-v3.ipynb
+# wget https://huggingface.co/openai/whisper-large-v3.ipynb
 wget https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B.ipynb
 wget https://huggingface.co/deepseek-ai/DeepSeek-R1-0528-Qwen3-8B.ipynb
-wget https://raw.githubusercontent.com/kyutai-labs/delayed-streams-modeling/main/tts_pytorch.ipynb
+# wget https://raw.githubusercontent.com/kyutai-labs/delayed-streams-modeling/main/tts_pytorch.ipynb
 wget https://huggingface.co/Qwen/Qwen3-Embedding-4B.ipynb
 wget https://huggingface.co/sentence-transformers/multi-qa-MiniLM-L6-cos-v1.ipynb
 wget https://huggingface.co/Qwen/Qwen2.5-VL-7B-Instruct.ipynb
